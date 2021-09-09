e9965911af656beba47fdc877523badef9ccb45b
Warren Joseph Ramos <wjnramos@gmail.com>
2021-09-09 08:13:25 -0600

    created the meals component to render the meals summary and available meals in the app

--- src/components/Meals/Meals.js
+++ src/components/Meals/Meals.js
@@ -1,6 +1,20 @@
 import React from 'react';
 
+import '../../utilities.css';
+import classes from './Meals.module.css';
+
+import AvailableMeals from './AvailableMeals';
+import MealsSummary from './MealsSummary';
+
 const Meals = () => {
+  return (
+    <div className={classes.meals}>
+      <div className='container'>
+        <MealsSummary />
+        <AvailableMeals />
+      </div>
+    </div>
+  );
 
 };
 
