1dafa90a0a3ec8795b07fbd9ca42e776cff29da0
Warren Joseph Ramos <wjnramos@gmail.com>
2021-09-09 08:11:06 -0600

    added a meals/menu summary section

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import Card from '../UI/Card';
 
 import classes from './MealsSummary.module.css';
 
 const MealsSummary = () => {
-
+  return (
+    <section className={classes['meals-summary']}>
+      <Card>
+        <h2>WR Menu</h2>
+        <hr />
+        <p>
+          Choose from a variety of excellent dishes, cooked by Michelin Star chefs using high-quality ingredients.
+        </p>
+        <p>
+          Today's Speciality: Glazed Strawberry Donuts!
+        </p>
+      </Card>
+    </section>
+  );
 };
 
 export default MealsSummary;
