123a3ddd19b1c9ed4a3799c42148d9a19aae0809
Warren Joseph Ramos <wjnramos@gmail.com>
2021-09-09 18:19:23 -0600

    added the effect to the cart button which would update the badge containing the number of items when a user adds or removes an item from the cart

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,16 +1,42 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 
 import classes from './HeaderCartButton.module.css';
 
+import CartContext from '../../store/cart-context';
+
 const HeaderCartButton = (props) => {
-  const btnIsHighlighted = false;
+  const cartContext = useContext(CartContext);
+  const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
+
+  const numberOfCartItems = cartContext.items.reduce(
+    (acc, item) => {
+      return acc + item.amount;
+    }, 0
+  );
+
+  const { items } = cartContext;
 
   const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ''}`;
+
+  useEffect(() => {
+    if (items.length === 0) {
+      return;
+    }
+    setBtnIsHighlighted(true);
+
+    const timer = setTimeout(() => {
+      setBtnIsHighlighted(false);
+    }, 300);
+
+    return () => {
+      clearTimeout(timer);
+    }
+  }, [items]);
 
   return (
     <button className={btnClasses} onClick={props.onClick} >
       <span>Your Cart</span>
-      <span className={classes.badge}>1</span>
+      <span className={classes.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
